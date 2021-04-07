077be8a2be91b7c52a8df0e1b5beb31ab117c4de
William W. Huang <williamhuang0623@gmail.com>
2021-04-07 19:19:44 -0400

    remove bad symlinks

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import HomePage from 'home/Index.js';
+import HomePage from 'home';
 import ExampleApi from 'lib/api/Example';
 
 export default function Home({ result }) {
