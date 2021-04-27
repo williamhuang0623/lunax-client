2bcfab928035753d94d2f54159bec779a5b4fc40
Pepi <pepimartinez@protonmail.com>
2021-04-27 06:29:57 -0400

    added dialog and theme

--- admin/index.js
+++ admin/index.js
@@ -1,12 +1,38 @@
-import React, { useState, useEffect, useLayoutEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { adminStyles } from './style';
 import Form from './orderform';
 import OrderList from './orderlist';
 import OrderAPI from '../lib/api/Order';
-
+import { ThemeProvider, makeStyles } from '@material-ui/styles';
+import { theme } from '../styles/admin-theme';
+import AddIcon from '@material-ui/icons/Add';
+import {
+    Container,
+    Typography,
+    Button,
+    Dialog,
+    DialogTitle,
+    DialogContent,
+} from '@material-ui/core';
+const useStyles = makeStyles((theme) => ({
+    order_list_header: {
+        display: 'flex',
+        flexDirection: 'row',
+        margin: 20,
+    },
+}));
 function AdminDashboard(props) {
     const [orders, setOrders] = useState([]);
+    const [open, setOpen] = React.useState(false);
+    const classes = useStyles();
 
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
     const handleSubmit = async (event) => {
         event.preventDefault();
 
@@ -24,23 +50,43 @@ function AdminDashboard(props) {
         const res = await orderapi.getAllOrders();
 
         setOrders([...res]);
-    }, orders);
+        setInterval(async () => {
+            const orderapi = new OrderAPI();
+            const res = await orderapi.getAllOrders();
 
-    const deleteOrder = async(order_id) => {
-        const deleted = await new OrderAPI().deleteOrder(order_id)
-        const newOrderList = orders.filter((item) => item._id !== deleted.deleted_id)
-        setOrders([...newOrderList])
-    }
+            setOrders([...res]);
+        }, 3000);
+    }, []);
+
+    const deleteOrder = async (order_id) => {
+        const deleted = await new OrderAPI().deleteOrder(order_id);
+        const newOrderList = orders.filter((item) => item._id !== deleted.deleted_id);
+        setOrders([...newOrderList]);
+    };
 
     return (
-        <>
+        <ThemeProvider theme={theme}>
             <div className="buffer" />
-            <div className="admin-container">
-                <Form handleSubmit={handleSubmit} />
+            <Container maxWidth="fixed">
+                <div className={classes.root}>
+                    <Typography color="primary" component="h3" variant="h3">
+                        Order List
+                    </Typography>
+                    <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+                        <AddIcon /> New
+                    </Button>
+                </div>
+
+                <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+                    <DialogTitle id="form-dialog-title">New Order</DialogTitle>
+                    <DialogContent>
+                        <Form handleSubmit={handleSubmit} />
+                    </DialogContent>
+                </Dialog>
                 <OrderList orders={orders} handleDelete={deleteOrder} />
-            </div>
+            </Container>
             <style jsx>{adminStyles}</style>
-        </>
+        </ThemeProvider>
     );
 }
 
