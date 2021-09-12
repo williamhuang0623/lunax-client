941e72886a0fca4a779fe4923fcb39124c88f054
pepimartinez <pepimartinez@protonmail.com>
2021-09-12 04:06:37 -0400

    contract addresses

---
+++ mainnet-config.js
@@ -0,0 +1,5 @@
+
+        export const _nftmarketaddress_mainnet = "0xA1936b4778b97c5C4C6f432498f0CC91607ada13"
+        export const _nftauctionaddress_mainnet = "0xb9267148949dDd0985d02b2a4C16FB37f4b02dcf"
+        export const _nftaddress_mainnet = "0x179955985C489827F76C7f13f6B0d36fb0A7697f"      
+        
