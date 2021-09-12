04360e57ba68ecd4ab03b8d70030c97545441138
pepimartinez <pepimartinez@protonmail.com>
2021-09-12 04:05:13 -0400

    new config for seperated local, testnet, mainnet

--- config.js
+++ config.js
@@ -1,6 +1,32 @@
+let nftaddress = "";
+let nftauctionaddress = "";
+let nftmarketaddress = "";
+const smart_contract_env = "mainnet";
 
-  export const nftmarketaddress = "0x6F99A55A24E49625598726a4964e78447052413F"
-  export const nftauctionaddress = "0x3BCC126fEEdb70870D1B5272dd0e78D7B53FC824"
-  export const nftaddress = "0xea7f8576E33C3A808637975D93F14580B8b49fCC"
+import { _nftmarketaddress_local, _nftauctionaddress_local, _nftaddress_local } from './local-config.js'
+import { _nftmarketaddress_testnet, _nftauctionaddress_testnet, _nftaddress_testnet } from './testnet-config.js'
+import { _nftmarketaddress_mainnet, _nftauctionaddress_mainnet, _nftaddress_mainnet } from './mainnet-config.js'
 
-  
+if (smart_contract_env == "local") {
+    nftmarketaddress = _nftmarketaddress_local
+    nftauctionaddress = _nftauctionaddress_local
+    nftaddress = _nftaddress_local
+}
+else if (smart_contract_env == "testnet") {
+    nftmarketaddress = _nftmarketaddress_testnet
+    nftauctionaddress = _nftauctionaddress_testnet
+    nftaddress = _nftaddress_testnet
+}
+else if (smart_contract_env == "mainnet") {
+    nftmarketaddress = _nftmarketaddress_mainnet
+    nftauctionaddress = _nftauctionaddress_mainnet
+    nftaddress = _nftaddress_mainnet
+}
+else {
+    console.log("WRONG/NULL SMART_CONTRACT_ENV!")
+}
+export {
+    nftaddress,
+    nftauctionaddress,
+    nftmarketaddress
+}
