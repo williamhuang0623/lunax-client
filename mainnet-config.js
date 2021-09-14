d033ef81bbf8de745eafbdc31b79bca1c10f94f3
pepimartinez <pepimartinez@protonmail.com>
2021-09-14 19:35:09 -0400

    latest contract addresses

--- mainnet-config.js
+++ mainnet-config.js
@@ -1,5 +1,5 @@
 
-        export const _nftmarketaddress_mainnet = "0x97Ae250956AAe605017155e05B777c277f1fa2DB"
-        export const _nftauctionaddress_mainnet = "0x20EE81DE4C4e7E5Ab02d714777235d92d24Df553"
-        export const _nftaddress_mainnet = "0xB08C7b1B736614Ee410598c5E2562eD638202A22"      
+        export const _nftmarketaddress_mainnet = "0x3BB29c38209633d3501368E572a04f1B516876cb"
+        export const _nftauctionaddress_mainnet = "0x05f9472E14A9501C43902BC561119d29B1a1F196"
+        export const _nftaddress_mainnet = "0x9d9DA1b94d7c96fa474589BE48e4B5d8A79A3C12"      
         
--- testnet-config.js
+++ testnet-config.js
@@ -1,5 +1,5 @@
 
-        export const _nftmarketaddress_testnet = "0xB08C7b1B736614Ee410598c5E2562eD638202A22"
-        export const _nftauctionaddress_testnet = "0xFE39b11ebE55eD35cd8Bd2Aa5B061098eDe41572"
-        export const _nftaddress_testnet = "0xC401746709b11876846fD69ddcdd26D65cE333da"      
+        export const _nftmarketaddress_testnet = "0xa7C68D0B77998C5B71FAEA96f9c10d1D8e2F5519"
+        export const _nftauctionaddress_testnet = "0xA0A98989Ae7864b1A4A5397A904f2CcD47FfbfD6"
+        export const _nftaddress_testnet = "0x9aC9054FB9ea02b6fb782a5ecc99df5A5C2F5Ebc"      
         
