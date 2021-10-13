4f07ebb79b90910e051a9f35fa26383b3f0164a8
pepimartinez <pepimartinez@protonmail.com>
2021-10-12 22:00:12 -0400

    deployed on testnet, ready for everyone to work on it

--- testnet-config.js
+++ testnet-config.js
@@ -1,3 +1,5 @@
-export const _nftmarketaddress_testnet = '0xa7C68D0B77998C5B71FAEA96f9c10d1D8e2F5519';
-export const _nftauctionaddress_testnet = '0xA0A98989Ae7864b1A4A5397A904f2CcD47FfbfD6';
-export const _nftaddress_testnet = '0x9aC9054FB9ea02b6fb782a5ecc99df5A5C2F5Ebc';
+
+        export const _nftmarketaddress_testnet = "0xFb9dc5a627a268193Bb025F93135FaB6e59c072F"
+        export const _nftauctionaddress_testnet = "0x2DbA4918bFC9f9Ffc6F04b0003A70354f8964ee1"
+        export const _nftaddress_testnet = "0x790Aa31D518b9144CcACf90d8be3D4885c263465"      
+        
